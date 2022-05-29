db914b90a7827a8b08be91dfed1dc6cde3952554
Tomas Garcia <tomasgarciadev@gmail.com>
2022-05-29 23:19:27 +0200

    Basic form and button funtionality setup using State

--- src/components/Finder.js
+++ src/components/Finder.js
@@ -1,12 +1,24 @@
-function Finder() {
+import { useState } from 'react';
+import axios from 'axios';
+
+const Finder = () => {
+  const [username, setUsername] = useState("");
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username)
+  };
+
   return (
     <div className="form-control" data-testid="finder-test">
       <form data-testid="finder-test-form">
         <input
+          value={username}
           placeholder="GitHub Username"
+          onChange={e => setUsername(e.target.value)}
         >
         </input>
-        <button className="btn">Search</button>
+        <button className="btn" onClick={handleSubmit}>Search</button>
       </form>
     </div>
   )
