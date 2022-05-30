fd24ead9bd6ffe62ba4936993d01b5d76f9150b8
Tomas Garcia <tomasgarciadev@gmail.com>
2022-05-30 12:09:52 +0200

    getLanguages method created for Usercard to collect languages from user repositories

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,5 +1,19 @@
 const UserCard = ({ repos }) => {
-  console.log(repos);
+  let languages = [];
+
+  const getLanguages = (array) => {
+    array.map(repo => {
+      if (repo.language !== null ) {
+      languages.push(repo.language)
+    };
+    return languages
+    })
+  }
+
+getLanguages(repos)
+
+console.log(languages)
+
   return (
     <div className="usercard" data-testid="usercard-test">
       <h2>Superman's favorite programming language is JavaScript</h2>
